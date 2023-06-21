861e52bab5e33b15161a75697b553a8c2b648cc3
Ethan011224 <126064049+Ethan011224@users.noreply.github.com>
2023-06-21 21:23:40 +0800

    Add files via upload

--- 111-2-R Project/project.r
+++ 111-2-R Project/project.r
@@ -1,6 +1,7 @@
 #rjava and remotes for installing "tabulizer"
-install.packages("rJava", "Remotes")
-library(rJava)
+install.packages("remotes")
+install.packages("ggplot2")
+install.packages("dplyr")
 library(remotes)
 
 #installing Tubalizer
@@ -9,6 +10,7 @@ remotes::install_github(c("ropensci/tabulizerjars", "ropensci/tabulizer"), INSTA
 #Library
 library(tabulizer)
 library(dplyr)
+library(ggplot2)
 
 #Vector of PDF file names
 pdf_files <- c("2020_Annual_IMB_Piracy_Report.pdf", "2021_Annual_IMB_Piracy_Report.pdf", "2022_Annual_IMB_Piracy_Report.pdf")
@@ -38,21 +40,35 @@ for(i in 1:length(pdf_files)){
   View(df_list[[i]])
 }
 
-#merge all dataframe into one for easy operation
+#merge all data frame into one for easy operation
 merged_df <- merge(merge(df_list[[1]], df_list[[2]], all = TRUE), df_list[[3]], all = TRUE)
 
 merged_df[is.na(merged_df) | merged_df == ""] <- 0
 
-header_order <- c("Location", sort(names(merged_df)[-which(names(merged_df) == "Location")]))
+header_order <- c("Location", sort(names(merged_df)[-
+
+which(names(merged_df) == "Location")]))
 merged_df <- merged_df[, header_order]
 
-combined_row <- apply(merged_df[c(9, 10), ], 2, max)
-merged_df <- merged_df[-c(9, 10), ]
-merged_df <- rbind(combined_row, merged_df)
+merged_df <- merged_df %>%  group_by(Location) %>%
+  summarise_all(max)
 
 merged_df <- merged_df[!(merged_df$Location == "Total at year end"), ]
+merged_df[, !(names(merged_df) %in% "Location")] <- lapply(merged_df[, !(names(merged_df) %in% "Location")], as.numeric)
 
 View(merged_df)
 
-#calculation
+#calculation and graph
+merged_df$Total <- rowSums(merged_df[, -1])
+final_df <- merged_df
+sum_row <- colSums(final_df[, -1])
+total_row <- c("Total of year", sum_row)
+final_df <- rbind(final_df, total_row)
+View(final_df)
 
+ggplot(data = merged_df) +
+  geom_point(aes(x = Total, y = Location)) +
+  labs(x = "Reported Incidents", y = "Country", title = "Piracy Activity By Country 2016-2022") +
+  scale_x_continuous(breaks = seq(min(0), max(200), by = 10))
+
+ggplot(data = )
